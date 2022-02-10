ee78101468f1d265708bf14f898f49142c103327
Paweł Grajewski <unionproject88@gmail.com>
2022-02-10 19:46:00 +0100

    add modal

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./styles.css";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
+import Card from "./components/Cart/Cart";
 
 export default function App() {
   return (
     <Fragment>
+      <Card />
       <Header />
       <main>
         <Meals />
