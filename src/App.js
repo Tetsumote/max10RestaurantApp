baedefe5ebcbb1696b8e83af0bc3737f9bdc2b71
Paweł Grajewski <unionproject88@gmail.com>
2022-02-11 19:50:23 +0100

    add context

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 
 import Header from "./components/Layout/Header";
-import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
 import Card from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 export default function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Card />
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Card onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
